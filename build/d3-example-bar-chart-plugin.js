89060718174c4dae5a58097e90fea1ab6cee1979
Kiernan McGowan <timmcgowan625@gmail.com>
2016-02-19 08:55:34 -0600

    0.1.2

--- build/d3-example-bar-chart-plugin.js
+++ build/d3-example-bar-chart-plugin.js
@@ -894,7 +894,7 @@
   var ascendingBisect = bisector(ascending);
   var bisectRight = ascendingBisect.right;
 
-  function range(start, stop, step) {
+  function sequence(start, stop, step) {
     start = +start, stop = +stop, step = (n = arguments.length) < 2 ? (stop = start, start = 0, 1) : n < 3 ? 1 : +step;
 
     var i = -1,
@@ -913,7 +913,7 @@
   var e2 = Math.sqrt(2);
   function ticks(start, stop, count) {
     var step = tickStep(start, stop, count);
-    return range(
+    return sequence(
       Math.ceil(start / step) * step,
       Math.floor(stop / step) * step + step / 2, // inclusive
       step
@@ -1076,7 +1076,7 @@
     var scale = ordinal().unknown(undefined),
         domain = scale.domain,
         ordinalRange = scale.range,
-        range$$ = [0, 1],
+        range = [0, 1],
         step,
         bandwidth,
         round = false,
@@ -1088,15 +1088,15 @@
 
     function rescale() {
       var n = domain().length,
-          reverse = range$$[1] < range$$[0],
-          start = range$$[reverse - 0],
-          stop = range$$[1 - reverse];
+          reverse = range[1] < range[0],
+          start = range[reverse - 0],
+          stop = range[1 - reverse];
       step = (stop - start) / Math.max(1, n - paddingInner + paddingOuter * 2);
       if (round) step = Math.floor(step);
       start += (stop - start - step * (n - paddingInner)) * align;
       bandwidth = step * (1 - paddingInner);
       if (round) start = Math.round(start), bandwidth = Math.round(bandwidth);
-      var values = range(n).map(function(i) { return start + step * i; });
+      var values = sequence(n).map(function(i) { return start + step * i; });
       return ordinalRange(reverse ? values.reverse() : values);
     }
 
@@ -1105,11 +1105,11 @@
     };
 
     scale.range = function(_) {
-      return arguments.length ? (range$$ = [+_[0], +_[1]], rescale()) : range$$.slice();
+      return arguments.length ? (range = [+_[0], +_[1]], rescale()) : range.slice();
     };
 
     scale.rangeRound = function(_) {
-      return range$$ = [+_[0], +_[1]], round = true, rescale();
+      return range = [+_[0], +_[1]], round = true, rescale();
     };
 
     scale.bandwidth = function() {
@@ -1143,7 +1143,7 @@
     scale.copy = function() {
       return band()
           .domain(domain())
-          .range(range$$)
+          .range(range)
           .round(round)
           .paddingInner(paddingInner)
           .paddingOuter(paddingOuter)
@@ -1893,13 +1893,13 @@
     return interpolateCubehelixLong;
   })(1);
 
-  function constant(x) {
+  function constant$1(x) {
     return function() {
       return x;
     };
   }
 
-  function number$1(x) {
+  function number$2(x) {
     return +x;
   }
 
@@ -1908,7 +1908,7 @@
   function deinterpolateLinear(a, b) {
     return (b -= (a = +a))
         ? function(x) { return (x - a) / b; }
-        : constant(b);
+        : constant$1(b);
   }
 
   function deinterpolateClamp(deinterpolate) {
@@ -1989,7 +1989,7 @@
     };
 
     scale.domain = function(_) {
-      return arguments.length ? (domain = map.call(_, number$1), rescale()) : domain.slice();
+      return arguments.length ? (domain = map.call(_, number$2), rescale()) : domain.slice();
     };
 
     scale.range = function(_) {
@@ -3865,7 +3865,7 @@
     return line_chart;
   }
 
-  var version = "0.1.1";
+  var version = "0.1.2";
 
   exports.version = version;
   exports.chart = constructor;
